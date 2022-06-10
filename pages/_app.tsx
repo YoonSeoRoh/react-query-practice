1e37f34abf6741faced9fe729aa2d62c2e55d87a
YoonSeoRoh <yoonseo1997@naver.com>
2022-06-10 15:35:52 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useState} from "react";
+import {QueryClient, QueryClientProvider} from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //QueryClient를 이용하여 queryClient 인스턴스 생성
+  //QueryClientProvider의 props로 queryClient 전달
+  //REactQueryDevltools -> 개발도구 -> 개발환경이 development(NODE_ENV=development)일때만 나타남
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default MyApp
